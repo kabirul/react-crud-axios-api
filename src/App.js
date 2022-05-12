e07d55ed77d7b59e76f7f6aac78e99952d1e7228
Kabirul Islam Mondal <kabirul05@gmail.com>
2022-05-12 22:24:23 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./style/global.css";
+
+import AddBlog from "./components/AddBlog";
+import Blog from "./components/Blog";
+import BlogsList from "./components/BlogsList";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+class App extends Component {
+  render() {
+    return ( 	  
+	   <div>
+	    <BrowserRouter>
+        <Navbar />
+        <div className="container mt-3">
+          <Switch>
+            <Route exact path={["/", "/blogs"]} component={BlogsList} />
+            <Route exact path="/add" component={AddBlog} />
+            <Route path="/blogs/:id" component={Blog} />
+          </Switch>
+        </div>
+		 <Footer />	
+		 </BrowserRouter> 
+      </div>	
+    );
+  }
+}
+
+export default App;
